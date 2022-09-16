2a0ffc120e9cbbb9cfa0143e5196cb2f2ab6f52e
Marina <zmgmarina@mail.ru>
2022-09-17 02:06:47 +0500

    Add Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
